e534d0d1e05b41291d0acb00867ed49d92cc6baa
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-20 14:16:05 -0300

    atualizado documentacao dos testes

--- src/test/test_server.py
+++ src/test/test_server.py
@@ -3,9 +3,16 @@ import pytest
 
 @pytest.fixture
 def resp():
+    """
+    Fixture que retorna uma resposta no endpoint raiz da API
+    """
     return client.get('/')
 
 
 def test_server(resp):
+    """
+    Testa se ao fazer uma requisição para o endpoint raiz é retornada status code 200 e a mensagem
+    Back-end Challenge 2021 🏅 - Space Flight News
+    """
     assert resp.status_code == 200
     assert resp.json == {'mensagem': 'Back-end Challenge 2021 🏅 - Space Flight News'}
