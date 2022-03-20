a279a7cd2e6ce09f7163e8af38fa897dcc0b0337
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-20 15:47:17 -0300

    criado schemas para news

---
+++ src/schemas/schemas.py
@@ -0,0 +1,16 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class News(BaseModel):
+    id: Optional[int] = None
+    featured: bool
+    title: str
+    url: str
+    imageUrl: str
+    newsSite: str
+    summary: str
+    publishedAt: str
+    launches: Optional[List]
+    events: Optional[List]
