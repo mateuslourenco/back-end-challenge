35ac65a3762866ac590b9a0fa2a5f9dd4b296e63
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-20 15:17:33 -0300

    criado teste para rota articles/{id_artigo} com metodo PUT

--- src/test/test_artigos.py
+++ src/test/test_artigos.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 def test_status_code_get_articles():
     """
-    Testa se ao fazer uma requisição get para o endpoint /articles/ é retornado código 200
+    Testa se ao fazer uma requisição GET para o endpoint /articles/ é retornado código 200
     """
     resp = client.get('/articles/')
     assert resp.status_code == 200
@@ -15,7 +15,7 @@ def test_status_code_get_articles():
 
 def test_buscar_artigo_por_id():
     """
-    Testa se ao fazer uma requisição get para o endpoint /articles/{id_artigo} é retornado código 200
+    Testa se ao fazer uma requisição GET para o endpoint /articles/{id_artigo} é retornado código 200
     """
     resp = client.get('/articles/1')
     assert resp.status_code == 200
@@ -23,7 +23,15 @@ def test_buscar_artigo_por_id():
 
 def test_adicionar_novo_artigo():
     """
-    Testa se ao fazer uma requisição post para o endpoint /articles/ é retornado código 200
+    Testa se ao fazer uma requisição POST para o endpoint /articles/ é retornado código 200
     """
     resp = client.post('/articles/')
+    assert resp.status_code == 200
+
+
+def test_editar_artigo():
+    """
+    Testa se ao fazer uma requisição PUT para o endpoint /articles/{id_artigo} é retornado código 200
+    """
+    resp = client.put('/articles/1')
     assert resp.status_code == 200
